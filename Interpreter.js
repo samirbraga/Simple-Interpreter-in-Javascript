adc225922371643668a312056936d805aa56814d
samirbraga <samirchavess@gmail.com>
2018-03-31 13:19:42 -0300

    Removing extra giles

--- Interpreter.js
+++ Interpreter.js
@@ -4,12 +4,12 @@ class Token {
 	constructor(type, value) {
 		this.type = type;
 		this.value = value;
-  }
-
+	}
+	
 	str() {
 		return `Token(${this.type}, ${this.value.toString()})`;
-  }
-
+	}
+	
 	repr() {
 		return this.str();
 	}
@@ -19,13 +19,13 @@ class Lexer {
 	constructor(text) {
 		this.text = text;
 		this.pos = 0;
-    this.currentChar = this.text[this.pos];
+		this.currentChar = this.text[this.pos];
 	}
-
+	
 	error() {
 		throw new Error('Error parsing input');
 	}
-
+	
 	advance() {
 		this.pos += 1;
 		if (this.pos > this.text.length - 1) {
@@ -34,13 +34,13 @@ class Lexer {
 			this.currentChar = this.text[this.pos];
 		}
 	}
-
+	
 	skipWhiteSpace () {
 		while (this.currentChar !== null && /\s/g.test(this.currentChar)) {
 			this.advance();
 		}
 	}
-
+	
 	integer() {
 		let result = '';
 		while (this.currentChar !== null && /[0-9]/g.test(this.currentChar)) {
@@ -49,14 +49,14 @@ class Lexer {
 		}
 		return parseInt(result);
 	}
-
+	
 	getNextToken() {
 		while (this.currentChar !== null) {
 			if (this.currentChar === ' ') {
 				this.skipWhiteSpace();
 				continue;
 			}
-
+			
 			if (/[0-9]/.test(this.currentChar)) {
 				return new Token(INTEGER, this.integer());
 			}
@@ -72,17 +72,17 @@ class Lexer {
 				this.advance();
 				return new Token(MUL, '*');
 			}
-
+			
 			if (this.currentChar === '/') {
 				this.advance();
 				return new Token(DIV, '/');
 			}
 			// if (this.currentChar === '^') {
 			// this.advance(); // return new Token(POW, '^'); // }
-
+			
 			this.error();
 		}
-
+		
 		return new Token(EOF, null);
 	}
 }
@@ -92,11 +92,11 @@ class Interpreter {
 		this.lexer = lexer;
 		this.currentToken = this.lexer.getNextToken();
 	}
-
+	
 	error() {
 		throw new Error('Error parsing input');
 	}
-
+	
 	eat(tokenType) {
 		if (this.currentToken.type == tokenType) {
 			this.currentToken = this.lexer.getNextToken();
@@ -104,31 +104,31 @@ class Interpreter {
 			this.error();
 		}
 	}
-
+	
 	factor() {
 		let token = this.currentToken;
 		this.eat(INTEGER);
 		return token.value;
 	}
-
+	
 	expr() {
-    let opers = {};
-    
+		let opers = {};
+		
 		// opers[PLUS] = (a, b) => a + b;
 		// opers[MINUS] = (a, b) => a - b;
 		opers[MUL] = (a, b) => a * b;
 		opers[DIV] = (a, b) => a / b;
-    // opers[POW] = (a, b) => Math.pow(a, b);
-
+		// opers[POW] = (a, b) => Math.pow(a, b);
+		
 		let result = this.factor();
-
+		
 		while ([MUL, DIV].includes(this.currentToken.type)) {
 			let token = this.currentToken;
-
-      this.eat(token.type);
-      result = opers[token.type](result, this.factor());
+			
+			this.eat(token.type);
+			result = opers[token.type](result, this.factor());
 		}
-
+		
 		return result;
 	}
 }
